a658c0fc7ee1c6974d52130f083cba34dcc19ab9
sdnshu <sudhanshuneravati077@gmail.com>
2025-10-08 11:13:48 +0530

    feat: add shadcn dashboard

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import { Sidebar } from '@/app/(root)/_components/sidebar'
+import { Header } from '@/app/(root)/_components/header'
+
+import {
+    SidebarInset,
+    SidebarProvider,
+} from "@/components/ui/sidebar"
+
+const Layout = ({
+    children,
+}: {
+    children: React.ReactNode
+}) => {
+    return (
+        <SidebarProvider
+            style={
+                {
+                    "--sidebar-width": "calc(var(--spacing) * 72)",
+                    "--header-height": "calc(var(--spacing) * 12)",
+                } as React.CSSProperties
+            }
+        >
+            <Sidebar variant="inset" />
+            <SidebarInset>
+                <Header />
+                <section>{children}</section>
+            </SidebarInset>
+        </SidebarProvider>
+    )
+}
+
+export default Layout
